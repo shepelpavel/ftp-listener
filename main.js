41d86c8421785ee75c5c57f024eb92747691ff1b
Shepel Pavel <pavel@shepel.net>
2021-08-17 15:03:19 +0300

    feat: fix 1 level folders

--- main.js
+++ main.js
@@ -77,22 +77,90 @@ ipcMain.on('getList', (event, options) => {
             compact: true,
             spaces: 2
         }))
-        var sites_arr = document.FileZilla3.Servers.Folder
-        for (var i1 = 0; i1 < sites_arr.length; i1++) {
-            var folder_name = sites_arr[i1]._text
-            _result.splice(i1, 0, {
+        if (document.FileZilla3.Servers.Folder) {
+            var sites_arr = document.FileZilla3.Servers.Folder
+            if (sites_arr.length > 1) {
+                for (var i1 = 0; i1 < sites_arr.length; i1++) {
+                    var folder_name = sites_arr[i1]._text
+                    _result.splice(i1, 0, {
+                        folder: folder_name,
+                        sites: []
+                    })
+                    if (sites_arr[i1].Server.length > 1) {
+                        for (var i2 = 0; i2 < sites_arr[i1].Server.length; i2++) {
+                            var site = {
+                                name: sites_arr[i1].Server[i2].Name,
+                                host: sites_arr[i1].Server[i2].Host,
+                                port: sites_arr[i1].Server[i2].Port,
+                                login: sites_arr[i1].Server[i2].User,
+                                pass: sites_arr[i1].Server[i2].Pass
+                            }
+                            _result[i1].sites.push(site)
+                        }
+                    } else {
+                        _result[0].sites.push({
+                            name: sites_arr[i1].Server.Name,
+                            host: sites_arr[i1].Server.Host,
+                            port: sites_arr[i1].Server.Port,
+                            login: sites_arr[i1].Server.User,
+                            pass: sites_arr[i1].Server.Pass
+                        })
+                    }
+                }
+            } else {
+                var folder_name = sites_arr._text
+                _result.splice(0, 0, {
+                    folder: folder_name,
+                    sites: []
+                })
+                if (sites_arr.Server.length > 1) {
+                    for (var i2 = 0; i2 < sites_arr.Server.length; i2++) {
+                        var site = {
+                            name: sites_arr.Server[i2].Name,
+                            host: sites_arr.Server[i2].Host,
+                            port: sites_arr.Server[i2].Port,
+                            login: sites_arr.Server[i2].User,
+                            pass: sites_arr.Server[i2].Pass
+                        }
+                        _result[0].sites.push(site)
+                    }
+                } else {
+                    _result[0].sites.push({
+                        name: sites_arr.Server.Name,
+                        host: sites_arr.Server.Host,
+                        port: sites_arr.Server.Port,
+                        login: sites_arr.Server.User,
+                        pass: sites_arr.Server.Pass
+                    })
+                }
+            }
+        }
+        if (document.FileZilla3.Servers.Server) {
+            var sites_arr = document.FileZilla3.Servers
+            var folder_name = 'No folder'
+            _result.splice(0, 0, {
                 folder: folder_name,
                 sites: []
             })
-            for (var i2 = 0; i2 < sites_arr[i1].Server.length; i2++) {
-                var site = {
-                    name: sites_arr[i1].Server[i2].Name,
-                    host: sites_arr[i1].Server[i2].Host,
-                    port: sites_arr[i1].Server[i2].Port,
-                    login: sites_arr[i1].Server[i2].User,
-                    pass: sites_arr[i1].Server[i2].Pass
+            if (sites_arr.Server.length > 1) {
+                for (var i1 = 0; i1 < sites_arr.Server.length; i1++) {
+                    var site = {
+                        name: sites_arr.Server[i1].Name,
+                        host: sites_arr.Server[i1].Host,
+                        port: sites_arr.Server[i1].Port,
+                        login: sites_arr.Server[i1].User,
+                        pass: sites_arr.Server[i1].Pass
+                    }
+                    _result[0].sites.push(site)
                 }
-                _result[i1].sites.push(site)
+            } else {
+                _result[0].sites.push({
+                    name: sites_arr.Server.Name,
+                    host: sites_arr.Server.Host,
+                    port: sites_arr.Server.Port,
+                    login: sites_arr.Server.User,
+                    pass: sites_arr.Server.Pass
+                })
             }
         }
         event.sender.send('sites_list', _result)
